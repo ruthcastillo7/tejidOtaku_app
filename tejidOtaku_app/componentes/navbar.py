cbe9d031e87682d764bded9605765e831b9dfe96
ruthcastillo22 <castillohuamaniruthm@gmail.com>
2024-12-05 10:26:47 -0500

    feed:agregue los tabs
    componentes

--- tejidOtaku_app/componentes/navbar.py
+++ tejidOtaku_app/componentes/navbar.py
@@ -6,6 +6,7 @@ def navbar()->rx.Component:
         rx.icon(tag="shopping-cart",color="#3d1d0b"),
         rx.heading("TejidOtaku",size="6",weight="bold",color="#3d1d0b"),
       ),
+      
       rx.hstack(
         rx.input(
             placeholder="Buscar",
@@ -17,7 +18,8 @@ def navbar()->rx.Component:
             rx.icon(tag="bell-dot"),
             justify="end",
             spacing="5"
-          ),background="#A0581D"
+          ),background="#A0581D",
+          #margin_left="20em"
         ),
         rx.button(
           rx.hstack(
